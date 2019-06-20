d7bc02c02c5f5b3ad65c60cf72c68aa27d919182
zhaoyang <365335093@qq.com>
2019-06-20 16:30:16 +0800

    Move user model to models folder

--- config/auth.php
+++ config/auth.php
@@ -70,6 +70,7 @@ return [
             'model' => App\Models\User::class,
         ],
 
+        
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
