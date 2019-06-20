a84902f4e28297863c7ea242854f40336708b34b
zhaoyang <365335093@qq.com>
2019-06-21 06:58:24 +0800

    、

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,8 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
-{
+class RedirectIfAuthenticated {
+
     /**
      * Handle an incoming request.
      *
@@ -15,12 +15,11 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
-    {
+    public function handle($request, Closure $next, $guard = null) {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            session()->flash('info', '您已登录，无需再次操作。');
+            return redirect('/');
         }
-
-        return $next($request);
     }
+
 }
